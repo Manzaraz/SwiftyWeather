4bd4fdebb3af669a843c56132a886ac7588f5a40
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-18 15:22:43 -0300

    6. Color:     - Color the area behind all views color .cyan at an opacity 75%     - Change all Views (including the gear button) so that they are isplayeded in the color .white, except for the Image, which should display in multicolored option

--- SwiftyWeather/WeatherView.swift
+++ SwiftyWeather/WeatherView.swift
@@ -10,35 +10,47 @@ import SwiftUI
 struct WeatherView: View {
     var body: some View {
         NavigationStack {
-            VStack(spacing: 0) {
-                Image(systemName: "cloud.sun.rain.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .padding(.horizontal)
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.cyan.opacity(0.75), Color.teal.opacity(0.75)]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                    .ignoresSafeArea()
                 
+                VStack(spacing: 0) {
+                    Image(systemName: "cloud.sun.rain.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.horizontal)
+                        .symbolRenderingMode(.multicolor)
                     
-                Text("Wild Weather")
-                    .font(.largeTitle)
-                
-                Text("42°F")
-                    .font(.system(size: 150, weight: .thin))
-                
-                Text("Wind 10mph - Feels Like 36°F")
-                    .font(.title2)
-                    .padding()
-            }
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        // TODO: Add gear click here
-                    } label: {
-                        Image(systemName: "gear")
+                    
+                    Text("Wild Weather")
+                        .font(.largeTitle)
+                    
+                    Text("42°F")
+                        .font(.system(size: 150, weight: .thin))
+                    
+                    Text("Wind 10mph - Feels Like 36°F")
+                        .font(.title2)
+                        .padding()
+                }
+                .foregroundStyle(.white)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            // TODO: Add gear click here
+                        } label: {
+                            Image(systemName: "gear")
+                        }
+                        .tint(.white)
                     }
-
                 }
             }
-            
         }
+        
+        
     }
 }
 
