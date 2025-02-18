76a3ab05d1aad49a94f0c62afeaf0f54f3480b65
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-18 11:28:07 -0300

    Initial Commit

---
+++ SwiftyWeather/SwiftyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyWeatherApp.swift
+//  SwiftyWeather
+//
+//  Created by Christian Manzaraz on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
